f742a2bf9f6d23f5730bd09c2468b2744569b015
David Becker <david@beckersite.com>
2020-01-29 17:03:11 -0700

    Toggle validation for input fields in create new user

--- src/Components/NewUserForm/NewUserForm.js
+++ src/Components/NewUserForm/NewUserForm.js
@@ -6,10 +6,13 @@ class NewUserForm extends React.Component {
         super()
         this.state = {
             username: '',
-            email: ''
+            email: '',
+            isValidInput: false
         }
-        this.createNewUser = this.createNewUser.bind(this)
+        this.createNewUserWithValidation = this.createNewUserWithValidation.bind(this)
         this.constructUser = this.constructUser.bind(this)
+        this.toggleValidation = this.toggleValidation.bind(this)
+        this.checkInputValidation = this.checkInputValidation.bind(this)
     }
 
     constructUser() {
@@ -20,26 +23,48 @@ class NewUserForm extends React.Component {
         }
     }
 
-    createNewUser(e) {
+    checkInputValidation() {
         const { username, email } = this.state
+        const usernameIsValid = !!username && username.length <= 30 && !parseInt(username[0])
+        const emailIsValid = !!email && email.length <= 30
+        return usernameIsValid && emailIsValid
+    }
+
+    catchUserInput(field, input) {
+        this.setState({
+            [field]: input
+        }, () => {
+            this.toggleValidation({ isValidInput: true })
+        })
+    }
+
+    toggleValidation({ isValidInput }) {
+        this.setState({
+            isValidInput
+        })
+    }
+
+    createNewUserWithValidation(e) {
         const { createNewUser } = this.props
         e.preventDefault()
-        if (!!username && !!email) {
+        if (this.checkInputValidation()) {
             const user = this.constructUser()
             createNewUser(user)
         } else {
-            console.log('need a user name and email')
+            this.toggleValidation({ isValidInput: false })
         }
     }
 
     render() {
-        const { username, email } = this.state
+        const { username, email, isValidInput } = this.state
         const { toggleCreateNewUser } = this.props
+        const submitDisabled = !username || !email
         return (
-            <form onSubmit={(e) => this.createNewUser(e)}>
-                <input type='text' placeholder='Username' value={username} onChange={(e) => this.setState({ username: e.target.value })} />
-                <input type='email' placeholder='Email' value={email} onChange={(e) => this.setState({ email: e.target.value })} />
-                <button type='submit'>Create New User</button>
+            <form onSubmit={(e) => this.createNewUserWithValidation(e)}>
+                <input type='text' placeholder='Username' value={username} onChange={(e) => this.catchUserInput('username', e.target.value)} />
+                <input type='email' placeholder='Email' value={email} onChange={(e) => this.catchUserInput('email', e.target.value)} />
+                <button type='submit' disabled={submitDisabled}>Create New User</button>
+                { !isValidInput && username && email && <p className='validation-error'>Please enter a valid username and email</p>}
                 <button onClick={() => toggleCreateNewUser()}>X</button>
             </form>
         )

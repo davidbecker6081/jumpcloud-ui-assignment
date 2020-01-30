58c679749c404b9d43d98dca028e27c939472703
David Becker <david@beckersite.com>
2020-01-29 20:37:14 -0700

    Test some reducers

--- DISCUSSION.md
+++ DISCUSSION.md
@@ -1,6 +1,5 @@
-- Add in ActionUseCase and ReducerUseCase as return data types
-- Extract methods as reducer utilities
-- Test with sinon
+- Add in ActionUseCase and ReducerUseCase as return data types for more structured and standardized return values 
+- Test with sinon for mocking API requests
 - Add ESLINT
 - Loading suspense should be a <Loading> component
 - Adding <ErrorBoundary> to catch errors, especially from API requests
@@ -9,6 +8,7 @@
 - Add some kind of toaster notification (timed popup notification that tells the user they successfully updated the user)
 - Reducer could definitely be extracted into more granular pieces (i.e some pretty good examples of a few potential refactors here: https://redux.js.org/recipes/structuring-reducers/refactoring-reducer-example)
     - That said, I don't think in this simple case, we really need to do that. But if we were making a large app, probably not a bad thing to do
+- Redux styleguide: https://redux.js.org/style-guide/style-guide
 
 1. List the users in your JumpCloud organization.
 2. Create a new user.
--- src/Reducers/test/userReducer.spec.js
+++ src/Reducers/test/userReducer.spec.js
@@ -1,9 +1,43 @@
 import UserReducer from '../userReducer'
+import {
+    GET_ALL_USERS,
+    TOGGLE_CREATE_NEW_USER,
+    CREATE_NEW_USER_SUCCESS,
+    CREATE_NEW_USER_ERRED,
+    CLEAR_ERROR,
+    TOGGLE_DELETE_USER,
+    DELETE_USER_SUCCESS,
+    DELETE_USER_ERRED,
+    TOGGLE_UPDATE_USER,
+    UPDATE_USER_SUCCESS,
+    UPDATE_USER_ERRED
+} from '../../Actions/utils/constants'
 
 describe('user reducer', () => {
+    const userA = { email: 'email@email.com', username: 'user1234' }
+    const userB = { email: 'emailB@email.com', username: 'user5678'}
+    const testUsers = [userA, userB]
+    const initialState = {
+        users: [],
+        createNewUserWindowOpen: false,
+        deleteUserConfirmation: false,
+        userToDelete: null,
+        userToUpdate: null,
+        status: 'OK'
+    }
     it('should have a default state', () => {
-        const action = { type: '', todo: '' }
-        const expectedReturn = { users: [] }
+        const action = { type: undefined }
+        const expectedReturn = initialState
         expect(UserReducer(undefined, action)).toEqual(expectedReturn)
+    })
+    it('should get all the users', () => {
+        const action = { type: GET_ALL_USERS, payload: { users: testUsers }}
+        const expectedReturn = { ...initialState, users: testUsers }
+        const actualReturn = UserReducer(undefined, action)
+        const userAFound = !!actualReturn.users.find(user => user.username === userA.username)
+        
+        expect(actualReturn).toEqual(expectedReturn)
+        expect(actualReturn.users.length).toEqual(2)
+        expect(userAFound).toEqual(true)
     })
 })
